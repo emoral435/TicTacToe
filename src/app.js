a982f5d39ee183b02c7b4e4a8a2bdf004b61d58d
Eduardo Morales <emoral435@gmail.com>
2022-11-21 19:25:29 -0600

    Started functionality checking for 3 in a row

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // This is where we store the gameboard and its state, using an array
 const Gameboard = (() => {
-    const board = []
-    return({board})
+    let board = new Array(9)
+    return{board}
 })();
 
 // this is where we have the players information stored, as an object
@@ -16,30 +16,94 @@ const Player = (symbol, id) => {
 
 const player1 = Player('X', 'Player1')
 const player2 = Player('O', 'Player2')
+let counter = 0
 
 // ControlGame module, that controls the flow of the game
 const ControlGame = ((player1, player2, Gameboard) => {
+    const winDiv = document.getElementById('Results')
     const start = () => {
         const board = document.querySelectorAll('.tictac')
         board.forEach( (square) => {
             square.addEventListener('click', () => {
+                addToArray(square)
                 doStart(square)
+                check()
+                counter+=1
             })
         })
     }
     const doStart = (square) => {
-        square.textContent = player1.symbol
+        if (square.textContent === '') {
+            if ((counter % 2) === 0) {
+                square.textContent = player1.symbol
+    
+            } else {
+                square.textContent = player2.symbol
+            } player1.getDiv().classList.toggle('toggle')
+            player2.getDiv().classList.toggle('toggle')
+        } 
     }
 
-    const clearEventBoard =(() => {
+    const clearEventsOnBoard =(() => {
         const board = document.querySelectorAll('.tictac')
         board.forEach( (square) => {
             let newSquare = square.cloneNode(true)
-            newSquare.textContent = ''
             square.parentNode.replaceChild(newSquare, square)
         })
     })
-    return {start, clearEventBoard}
+
+    const addToArray = (square) => {
+        if ((counter % 2) === 0) {
+            Gameboard.board[parseInt(square.dataset.id)] = true
+        } else {
+            Gameboard.board[parseInt(square.dataset.id)] = false
+        }
+    }
+
+    const checkforTrue = (array) => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === true) {
+                return true
+            }
+        }
+    }
+    const check = () => {
+        if ((counter % 2) === 0) {
+            let o1 = Gameboard.board[0] && Gameboard.board[1] && Gameboard.board[2]
+            let o2 = Gameboard.board[3] && Gameboard.board[4] && Gameboard.board[5]
+            let o3 = Gameboard.board[6] && Gameboard.board[7] && Gameboard.board[8]
+            let o4 = Gameboard.board[0] && Gameboard.board[3] && Gameboard.board[6]
+            let o5 = Gameboard.board[1] && Gameboard.board[4] && Gameboard.board[7]
+            let o6 = Gameboard.board[2] && Gameboard.board[5] && Gameboard.board[8]
+            let o7 = Gameboard.board[2] && Gameboard.board[4] && Gameboard.board[6]
+            let o8 = Gameboard.board[0] && Gameboard.board[4] && Gameboard.board[8]
+            let checkArray = [o1, o2, o3, o4, o5, o6, o7, o8]
+            if (checkforTrue(checkArray)) {
+                winDiv.textContent = 'Player 1 Won!'
+                clearEventsOnBoard()
+            }
+        }
+        // else if (counter === 9) {
+        //     clearEventsOnBoard()
+        // }
+    }
+
+    return {start, clearEventsOnBoard}
 })(player1, player2, Gameboard)
 
+// reset function button call
+const reset = () => {
+    const board = document.querySelectorAll('.tictac')
+    board.forEach( btn => {
+        btn.textContent = ''
+        if ((counter % 2) != 0) {
+            player1.getDiv().classList.toggle('toggle')
+            player2.getDiv().classList.toggle('toggle')
+        }
+        counter = 0
+    })
+    ControlGame.start()
+}
+
+// MAIN
 ControlGame.start()
